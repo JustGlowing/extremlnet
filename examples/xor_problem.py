9208723b6bbc2479811be31b84a6e1eb9e6a2f62
justglowing <vettigli@gmail.com>
2015-06-15 09:57:57 +0100

    fixed xor_problem.py output

--- examples/xor_problem.py
+++ examples/xor_problem.py
@@ -7,9 +7,9 @@ import scipy as sp
 if __name__ == '__main__':
     x = np.array([[0.0,0.0],[0.0,1.0],[1.0,0.0],[1.0,1.0]])
     y = np.array([1.0,0.0,0.0,1.0])
-    net = ExtrmlNet(1) # can't solve this problem with 1 hidden neuron
-    net.fit(x,y)
-    print np.round(net.predict(x)), ' =', y
-    net = ExtrmlNet(4) # with 4 we can
-    net.fit(x,y)
+    net = ExtrmlNet(1)  # can't solve this problem with 1 hidden neuron
+    net.fit(x, y)
     print np.round(net.predict(x)), '!=', y
+    net = ExtrmlNet(4)  # with 4 we can
+    net.fit(x, y)
+    print np.round(net.predict(x)), ' =', y
